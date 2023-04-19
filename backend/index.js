e348c512fb52ccce2754fca4d6eda889cc6cef1c
David Boxler <david.boxler@incluit.com>
2023-04-19 13:31:44 -0300

    se sube carpeta del proyecto

---
+++ backend/index.js
@@ -0,0 +1,160 @@
+const express = require("express");
+const cors = require("cors")
+
+// Mensaje de conexion
+console.log("API REST conectada")
+
+// Crear servidor node
+const app = express()
+const puerto = 3900
+
+// Configurar cors
+app.use(cors())
+
+// Convertir los datos del body a objetos js
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+
+const originalEndpoints = {
+    search: "https://api.mercadolibre.com/sites/MLA/search?limit=4&q=",
+    items: "https://api.mercadolibre.com/items/",
+    categories: "https://api.mercadolibre.com/categories/",
+  };
+
+/* GET home page. */
+app.get("/api/items/:query", async (req, res) => {
+    const query = req.params.query;
+  
+    const fetchData = async () => {
+      const url = originalEndpoints.search + query;
+      try {
+        const respuesta = await fetch(url);
+        const data = await respuesta.json();
+        return data;
+      } catch (error) {
+        console.log("error", error.message);
+      }
+    };
+  
+    const data = await fetchData();
+  
+    const getCategories = async (data) => {
+      if (data.results[0].category_id) {
+        const respuestaCategoria = await fetch(
+          originalEndpoints.categories + data.results[0].category_id
+        );
+        const infoCat = await respuestaCategoria.json();
+  
+        return infoCat.path_from_root;
+      } else {
+        const respuestaCategoria = await fetch(
+          originalEndpoints.categories + data.available_filters[0].values[0].id
+        );
+        const infoCat = await respuestaCategoria.json();
+        return infoCat.path_from_root;
+      }
+    };
+  
+    const getItems = (data) => {
+      const itemsInfo = data.results.map((result) => {
+        return {
+          id: result.id,
+          title: result.title,
+          price: {
+            currency: result.currency_id,
+            amount: result.price,
+            decimals: "",
+          },
+          picture: result.thumbnail,
+          condition: result.condition,
+          free_shippin: result.shipping.free_shipping,
+          location: result.address.state_name
+        };
+      });
+  
+      return itemsInfo;
+    };
+  
+    const categories = await getCategories(data);
+    const items = getItems(data);
+  
+    res.json({
+      categories: categories,
+      items: items,
+    });
+  });
+
+  app.get("/api/items/:id", async (req, res) => {
+    const itemId = req.params.id;
+  
+    const fetchItemData = async () => {
+      const url = originalEndpoints.items + itemId;
+      try {
+        const respuesta = await fetch(url);
+        const data = await respuesta.json();
+        return data;
+      } catch (error) {
+        console.log("error", error.message);
+      }
+    };
+  
+    const fetchItemDescription = async () => {
+      const url = originalEndpoints.items + itemId + "/description";
+      try {
+        const respuesta = await fetch(url);
+        const data = await respuesta.json();
+        return data;
+
+      } catch (error) {
+        console.log("error", error.message);
+      }
+    };
+  
+    const getCategories = async (data) => {
+      const respuestaCategoria = await fetch(
+        originalEndpoints.categories + itemInfo.category_id
+      );
+      const infoCat = await respuestaCategoria.json();
+  
+      return infoCat.path_from_root;
+    };
+  
+    const itemInfo = await fetchItemData();
+    const itemDescription = await fetchItemDescription();
+    const itemCategories = await getCategories();
+  
+    const organizedItemInfo = {
+      id: itemInfo.id,
+      title: itemInfo.title,
+      price: {
+        currency: itemInfo.currency_id,
+        amount: itemInfo.price,
+      },
+      picture: itemInfo.thumbnail,
+      condition: itemInfo.condition,
+      free_shipping: itemInfo.shipping.free_shipping,
+      sold_quantity: itemInfo.sold_quantity,
+      description: itemDescription.plain_text,
+      categories: itemCategories,
+      seller_address: itemInfo.seller_address,
+      available_quantity: itemInfo.available_quantity
+    };
+  
+    res.json({
+      item: organizedItemInfo,
+    });
+  });
+
+// Ruta de prueba
+app.get("/ruta-prueba", (req, res) => {
+    return res.status(200).json({
+        "id": 1,
+        "nombre": "David",
+        "web": "davidboxler.es"
+    })
+})
+
+// Poner servidor a escuchar peticiones http
+app.listen(puerto, () => {
+    console.log("Servidor de node corriendo en el puerto: ", puerto)
+})
