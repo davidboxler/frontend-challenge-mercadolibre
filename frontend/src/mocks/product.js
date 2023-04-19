ab97e8379f7f68f29b2436be8af95bb30fd1bfa0
David Boxler <david.boxler@incluit.com>
2023-04-19 13:51:39 -0300

    se suben assets and mocks

---
+++ frontend/src/mocks/product.js
@@ -0,0 +1,302 @@
+const data = {
+    questions: [
+        {
+          question: "Hola tenes en un metro?",
+          answer: "Hola si tenemos stock",
+          answerDate: "13/01/2022"
+        },
+        {
+          question: "Hoy esta abierto para poder pasar a verlo?",
+          answer: "Hola, no. Saludos!",
+          answerDate: "24/12/2021"
+        },
+        {
+          question: "Hola tiene disponible para compra inmediata? Envío a Córdoba",
+          answer: "Que tal, si tenemos stock!",
+          answerDate: "21/12/2021"
+        },
+        {
+          question: "Si lo compro hoy para cuando masomenos llegaría? Soy de Santa Cruz",
+          answer: "Hola antes de la compra te figura que día te llega, por lo general 3-4 días hábiles o menos según el correo",
+          answerDate: "18/11/2021"
+        },
+        {
+          question: "Viene armado o hay que ponerle el relleno?",
+          answer: "Hola si tenemos stock, viene armado",
+          answerDate: "18/11/2021"
+        },
+        {
+          question: "Si lo compro el sábado, llega el mismo día?",
+          answer: "Hola, sí, por Mercado Envíos!",
+          answerDate: "02/11/2021"
+        },
+    ],
+    attributes: [
+      {
+        id: "MAX_RECOMMENDED_AGE",
+        name: "Edad máxima recomendada",
+        value_id: null,
+        value_name: "99 años",
+        value_struct: {
+          number: 99,
+          unit: "años",
+        },
+        values: [
+          {
+            id: null,
+            name: "99 años",
+            struct: {
+              number: 99,
+              unit: "años",
+            },
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "PRODUCT_TYPE",
+        name: "Tipo de producto",
+        value_id: null,
+        value_name: "Peluche",
+        value_struct: null,
+        values: [
+          {
+            id: null,
+            name: "peluche",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+      {
+        id: "ANIMAL",
+        name: "Animal",
+        value_id: null,
+        value_name: "626",
+        value_struct: null,
+        values: [
+          {
+            id: null,
+            name: "626",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "BRAND",
+        name: "Marca",
+        value_id: "33243",
+        value_name: "Disney",
+        value_struct: null,
+        values: [
+          {
+            id: "33243",
+            name: "Disney",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+      {
+        id: "CHARACTER",
+        name: "Personaje",
+        value_id: "4959202",
+        value_name: "Stitch",
+        value_struct: null,
+        values: [
+          {
+            id: "4959202",
+            name: "Stitch",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+      {
+        id: "FRANCHISE",
+        name: "Franquicia",
+        value_id: null,
+        value_name: "Lilo Y Stitch",
+        value_struct: null,
+        values: [
+          {
+            id: null,
+            name: "Stitch",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+      {
+        id: "GTIN",
+        name: "Código universal de producto",
+        value_id: null,
+        value_name: "8480017059031",
+        value_struct: null,
+        values: [
+          {
+            id: null,
+            name: "8480017059031",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "HEIGHT",
+        name: "Altura",
+        value_id: null,
+        value_name: "100 cm",
+        value_struct: {
+          number: 100,
+          unit: "cm",
+        },
+        values: [
+          {
+            id: null,
+            name: "100 cm",
+            struct: {
+              number: 100,
+              unit: "cm",
+            },
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+      {
+        id: "IS_HYPOALLERGENIC_MATERIAL",
+        name: "Es material hipoalergénico",
+        value_id: "242085",
+        value_name: "Sí",
+        value_struct: null,
+        values: [
+          {
+            id: "242085",
+            name: "Sí",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "ITEM_CONDITION",
+        name: "Condición del ítem",
+        value_id: "2230284",
+        value_name: "Nuevo",
+        value_struct: null,
+        values: [
+          {
+            id: "2230284",
+            name: "Nuevo",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "MATERIAL",
+        name: "Material",
+        value_id: "462324",
+        value_name: "Plush",
+        value_struct: null,
+        values: [
+          {
+            id: "462324",
+            name: "Plush",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+      {
+        id: "MIN_RECOMMENDED_AGE",
+        name: "Edad mínima recomendada",
+        value_id: null,
+        value_name: "0 años",
+        value_struct: {
+          number: 0,
+          unit: "años",
+        },
+        values: [
+          {
+            id: null,
+            name: "0 años",
+            struct: {
+              number: 0,
+              unit: "años",
+            },
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "MODEL",
+        name: "Modelo",
+        value_id: null,
+        value_name: "Stitch Grande",
+        value_struct: null,
+        values: [
+          {
+            id: null,
+            name: "Stitch",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "SELLER_SKU",
+        name: "SKU",
+        value_id: null,
+        value_name: "4340691960334",
+        value_struct: null,
+        values: [
+          {
+            id: null,
+            name: "4340691960334",
+            struct: null,
+          },
+        ],
+        attribute_group_id: "OTHERS",
+        attribute_group_name: "Otros",
+      },
+      {
+        id: "WIDTH",
+        name: "Ancho",
+        value_id: null,
+        value_name: "50 cm",
+        value_struct: {
+          number: 50,
+          unit: "cm",
+        },
+        values: [
+          {
+            id: null,
+            name: "50 cm",
+            struct: {
+              number: 50,
+              unit: "cm",
+            },
+          },
+        ],
+        attribute_group_id: "",
+        attribute_group_name: "",
+      },
+    ]
+}
+
+export default data;
